dae0add8ada4a3705f316f79354c26e57da59668
Michael Hudson <michaelhudson2205@gmail.com>
2025-04-04 16:18:19 +1100

    Finished 129 Scatter Plots.

--- demo_121_pair_plots.py
+++ demo_121_pair_plots.py
@@ -15,3 +15,7 @@ sns.histplot(student_data['Cups of Coffee'], bins=11, color='green')
 student_data['Hours Studied']
 student_data['Hours Studied'].round().value_counts().sort_index()
 sns.histplot(student_data['Hours Studied'], bins=5, color='purple')
+
+# 129 DEMO Scatter Plots
+sns.scatterplot(data=student_data, x='Hours of Sleep', y='Grade on Test')
+student_data[student_data['Hours of Sleep'] < 3]
